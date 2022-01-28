85dd88354265265bf76626e67bf640c9f272351b
shahidul islam <islamkhans148@gmail.com>
2022-01-28 16:50:03 +0600

    database chage

--- login_example/settings.py
+++ login_example/settings.py
@@ -70,12 +70,8 @@ WSGI_APPLICATION = 'login_example.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crud_login',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
